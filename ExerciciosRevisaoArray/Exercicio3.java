d0de7292054f0dc6341144e572f9402b5a893617
nickolasg1108 <nickolasgbol@gmail.com>
2025-09-26 09:30:03 -0300

    Update and rename Media.java to Exercicio3.java

--- ExerciciosRevisaoArray/Exercicio3.java
+++ ExerciciosRevisaoArray/Exercicio3.java
@@ -1,6 +1,6 @@
 import java.util.Scanner; 
 
-public class Media {
+public class Exercicio3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
@@ -22,4 +22,5 @@ public class Media {
         sc.close();
     }
 } 
+
 
